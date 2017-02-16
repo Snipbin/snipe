6aba89f8ea7ba080bc10dd825b2d44929a6cf19e
Ranveer Aggarwal <ranveeraggarwal@gmail.com>
2017-02-16 13:39:02 +0530

    Changes to Snippet Model

--- apps/snippet/models.py
+++ apps/snippet/models.py
@@ -4,9 +4,29 @@ from simple_history.models import HistoricalRecords
 from apps.account.models import UserProfile
 
 
+LANGUAGE_CHOICES = (
+    (1, "C#"),
+    (2, "PowerShell"),
+    (3, "C++"),
+    (4, "Python"),
+    (5, "Rust"),
+    (6, "JavaScript"),
+    (7, "CSS"),
+)
+
+
 class Snippet(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.CharField(max_length=256)
+    language = models.IntegerField(choices=LANGUAGE_CHOICES, default=1)
     code = models.TextField()
+    author = models.ForeignKey(UserProfile, related_name="snippet")
+    uid = models.CharField(max_length=32)
+    is_private = models.BooleanField(default=False)
     created = models.DateTimeField()
     last_modified = models.DateTimeField()
+    expiry_date = models.DateTimeField()
     _history_ = HistoricalRecords()
-    author = models.ForeignKey(UserProfile, related_name="snippet")
+
+    def __str__(self):
+        return self.title

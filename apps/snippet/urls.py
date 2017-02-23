c2d1aeba6bd95a93dcccecb496ef7a5568febec0
Ranveer Aggarwal <ranveeraggarwal@gmail.com>
2017-02-23 17:38:58 +0530

    Changed URL behaviour

--- apps/snippet/urls.py
+++ apps/snippet/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from apps.snippet.views.snippet import Snippet
 
 urlpatterns = [
-    url(r'^$', Snippet.as_view(), name='snippet'),
+    url(r'$', Snippet.as_view(), name='snippet'),
 ]

565c8c84e5430fda64ef98ea9a8d41bac2d95149
Ranveer Aggarwal <ranveeraggarwal@gmail.com>
2017-02-25 13:01:39 +0530

    Discovery: Minor bootstrap enhancements

--- snipe/urls.py
+++ snipe/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^', include('apps.core.urls')),
+    url(r'^', include('apps.core.urls', namespace='core')),
     url(r'^admin/', admin.site.urls),
     url(r'^(?P<username>[\w{}.-]+)/(?P<uid>[\w{}.-]+)/', include('apps.snippet.urls', namespace='snippet')),
     url(r'^(?P<username>[\w{}.-]+)/', include('apps.account.urls', namespace='account')),

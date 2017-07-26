cebcf142d221a90ebf2ffa3a3155dc85b103d20d
Dheerendra Rathor <dheeru.rathor14@gmail.com>
2017-07-26 12:17:05 +0530

    Updated run.cmd to honor continous deployment on azure

--- run.cmd
+++ run.cmd
@@ -1,5 +1,6 @@
 REM Azure webjob to sync search
 
+cd D:\home\site\wwwroot
 set DJANGO_SETTINGS_MODULE=snipe.settings.production
 D:\home\python361x64\python.exe -m pip install -r requirements/production.txt
 
